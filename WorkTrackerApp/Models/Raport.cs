a4ee42fe8823a81a391c08d46a41f4fa9b27dbd6
ŁUKASZ JANKOWSKI <lukasz.jankowski95@gmail.com>
2019-08-07 12:37:47 +0200

    Added model for Raports

---
+++ WorkTrackerApp/Models/Raport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WorkTrackerApp.Models
+{
+    public class Raport
+    {
+        public string Id { get; set; }
+        public string Company { get; set; }
+        public DateTime Date { get; set; }
+        public int WorkedTime { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ WorkTrackerApp/Services/SQLiteDataStore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorkTrackerApp.Models;
+
+namespace WorkTrackerApp.Services
+{
+    public class SQLiteDataStore : IDataStore<Raport>
+    {
+        public SQLiteDataStore()
+        {
+        }
+
+        public Task<bool> AddItemAsync(Raport item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Raport> GetItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Raport>> GetItemsAsync(bool forceRefresh = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateItemAsync(Raport item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
